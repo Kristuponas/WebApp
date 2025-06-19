dfbf27c3542fea20cec41f2e40759734c97a9e86
Kristuponas <149258526+Kristuponas@users.noreply.github.com>
2025-06-19 22:11:53 +0300

    Initial files for start

---
+++ frontend/frontend/src/components/Login.vue
@@ -0,0 +1,33 @@
+.<template>
+  <div class="wrapper">
+    <form action="">
+        <h1>Login</h1>
+        <div class="input-box">
+            <input type="text" placeholder="Username" required>
+        </div>
+        <div>
+            <input type="password" placeholder="Password" required>
+        </div>
+        <div class="remember-forgot">
+            <label><input type="checkbox"> Remember me </label>
+            <a href="#">Forgot password?</a>
+        </div>
+
+        <button type="submit" class="btn">Login</button>
+
+        <div class="register-link">
+            <p>Don't have an account? <a href="#">Register</a></p>
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
